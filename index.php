4e5282182249ea3c4c75ccaa1c7db765d5746d46
sapostologlou <s_apostologlou@hotmail.com>
2014-12-06 17:37:46 +0200

    login php

---
+++ index.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link rel='stylesheet' type='text/css' href='css/stylelogin.css' />
+    <title> offers</title>
+</head>
+<body >
+<div class='header' >
+    <h1>wellcome><br> to offers page</h1>
+</div>
+<div id='center_box'  >
+    <div id='image'>
+    </div>
+    <div id="log_in">
+        <form action="php/LoginClass.php" method="post" class="form-signin">
+            <fieldset>
+                <input class="form-control" name="ShopName" placeholder="Shop Name" id="username" type="text"><br>
+                <input class="form-control" name="Password" placeholder="Password" id="password" type="password">
+                <br><br>
+                <input type="submit" id="btnLoginIndex" value="Login" class="styleBtnIndex">
+                <input type="button" id="btnSignupIndex" value="SignUp" class="styleBtnIndex" onclick="window.location.href='register.php'">
+            </fieldset>
+        </form>
+    </div>
+    <div id="radio">
+        <input type='checkbox' checked='checked' />Remeber the password <br />
+    </div>
+</div>
+<div class='footer'>
+    <p><b> Thank you for your choise!!!</b></p>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
